2bca760ab77ab7e4ac808fcddbf3d07508621f9d
Apoorv Sarang <44388565+WizardOfGryfindor@users.noreply.github.com>
2019-03-14 00:22:11 -0700

    Music Player with music fetched from device storage

---
+++ app/src/main/java/com/example/musicplayer/SongInfo.java
@@ -0,0 +1,26 @@
+package com.example.musicplayer;
+
+public class SongInfo {
+    public String songName, artistName, songUrl;
+
+    public SongInfo() {
+    }
+
+    public SongInfo(String songName, String  artistName, String songUrl) {
+        this.songName = songName;
+        this.artistName = artistName;
+        this.songUrl = songUrl;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getSongUrl() {
+        return songUrl;
+    }
+}
